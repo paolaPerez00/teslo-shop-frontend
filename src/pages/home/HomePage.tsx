30b0287953d4100f74e6a5b49c5bfe33ef96cdd9
gpere13 <gpere13@bancodebogota.com.co>
2026-07-25 09:30:34 -0500

    Home Page parameters in URL

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,14 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mock/products.mocks"
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
 
 export const HomePage = () => {
     return (
         <>
             <CustomJumbotron title="Todos los productos"></CustomJumbotron >
+            <ProductsGrid products={products}></ProductsGrid >
+            <CustomPagination totalPages={5}></CustomPagination >
         </>
     )
 }
