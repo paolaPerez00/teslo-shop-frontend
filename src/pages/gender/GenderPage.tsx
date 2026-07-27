9b836f7d3a655c8646045999e4997236cf851f72
gpere13 <gpere13@bancodebogota.com.co>
2026-07-27 16:12:53 -0500

    screens navigate

--- src/pages/gender/GenderPage.tsx
+++ src/pages/gender/GenderPage.tsx
@@ -1,9 +1,23 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mock/products.mocks"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+    const { gender } = useParams();
+
+    const genderLabel = gender === 'men'
+        ? 'Hombres'
+        : gender === 'women'
+            ? 'Mujeres' : 'Niños';
     return (
-        <div>
-            Gneder PAGE
-        </div>
+        <>
+            <CustomJumbotron title={`Productos para ${genderLabel}`}></CustomJumbotron >
+            <ProductsGrid products={products}></ProductsGrid >
+            <CustomPagination totalPages={5}></CustomPagination >
+        </>
     )
 }
 
--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,12 +1,13 @@
-import { Search, ShoppingBag, Menu } from "lucide-react";
+import { Search, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useRef, useState } from "react";
-import { useSearchParams } from "react-router";
+import { useRef } from "react";
+import { Link, useParams, useSearchParams } from "react-router";
+import { cn } from "@/lib/utils";
 const CustomHeader = () => {
-    const [cartCount] = useState(3);
 
     const [searchParams, setSearchParams] = useSearchParams();
+    const { gender } = useParams();
 
     const inputRef = useRef<HTMLInputElement>(null);
     const query = searchParams.get('query');
@@ -38,18 +39,26 @@ const CustomHeader = () => {
 
                 {/* Navigation - Desktop */}
                 <nav className="hidden md:flex items-center space-x-8">
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Camisetas
-                    </a>
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Sudaderas
-                    </a>
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Chaquetas
-                    </a>
-                    <a href="#" className="text-sm font-medium transition-colors hover:text-primary">
-                        Accesorios
-                    </a>
+                    <Link to="/" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                        !gender ? 'underline underline-offset-4' : ''
+                    )}>
+                        Todos
+                    </Link>
+                    <Link to="/gender/men" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                        gender === 'men' ? 'underline underline-offset-4' : ''
+                    )}>
+                        Hombres
+                    </Link>
+                    <Link to="/gender/women" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                        gender === 'women' ? 'underline underline-offset-4' : ''
+                    )}>
+                        Mujeres
+                    </Link>
+                    <Link to="/gender/kid" className={cn(`text-sm font-medium transition-colors hover:text-primary`,
+                        gender === 'kid' ? 'underline underline-offset-4' : ''
+                    )}>
+                        Niños
+                    </Link>
                 </nav>
 
                 {/* Search and Cart */}
@@ -70,13 +79,19 @@ const CustomHeader = () => {
                     <Button variant="ghost" size="icon" className="md:hidden">
                         <Search className="h-5 w-5" />
                     </Button>
+                    <Link to='/auth/login'>
+                        <Button variant="default" size="sm" className="ml-2">
+                            Login
+                        </Button>
+                    </Link>
 
-                    <Button variant="ghost" size="icon" className="relative">
-                        <ShoppingBag className="h-5 w-5" />
-                        {cartCount > 0 && <span className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-primary-foreground text-xs flex items-center justify-center">
-                            {cartCount}
-                        </span>}
-                    </Button>
+                    <Link to='/admin'>
+                        <Button variant="destructive" size="sm" className="ml-2">
+                            Admin
+                        </Button>
+                    </Link>
+
+
                 </div>
             </div>
         </div>
